5622894478c6d31b98fa1a170c9077a9799355f9
Daniel Bendel <daniel.bendel@gmail.com>
2016-05-30 20:49:42 +0200

    chore(build): add sourcemaps

--- build/tasks/build.js
+++ build/tasks/build.js
@@ -10,6 +10,8 @@ var insert = require('gulp-insert');
 var rename = require('gulp-rename');
 var tools = require('aurelia-tools');
 var del = require('del');
+var sourcemaps = require('gulp-sourcemaps');
+
 var vinylPaths = require('vinyl-paths');
 
 var jsName = paths.packageName + '.js';
@@ -56,7 +58,9 @@ gulp.task('build-amd', function () {
 
 gulp.task('build-system', function () {
   return gulp.src(paths.source)
+    .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(to5(assign({}, compilerOptions, {modules:'system', plugins: []})))
+    .pipe(sourcemaps.write(paths.output + 'system'))
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
